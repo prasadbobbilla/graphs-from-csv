f82ef9e927828df30156e62dc404c6b448e123e7
prasadbobbilla <54373689+prasadbobbilla@users.noreply.github.com>
2020-11-12 02:00:23 -0600

    updated files

--- javascripts/create-graph.js
+++ javascripts/create-graph.js
@@ -2,7 +2,7 @@
  * Parse the data and create a graph with the data.
  */
 function parseData(createGraph) {
-	Papa.parse("../data/spanish-silver.csv", {
+	Papa.parse("../data/test2.csv", {
 		download: true,
 		complete: function(results) {
 			createGraph(results.data);
@@ -10,33 +10,77 @@ function parseData(createGraph) {
 	});
 }
 
-function createGraph(data) {
-	var years = [];
-	var silverMinted = ["Silver Minted"];
+// function createGraph(data) {
+// 	var years = [];
+// 	var silverMinted = ["situados_paid"];
 
-	for (var i = 1; i < data.length; i++) {
-		years.push(data[i][0]);
-		silverMinted.push(data[i][2]);
-	}
+// 	for (var i = 1; i < data.length; i++) {
+// 		years.push(data[i][0]);
+// 		silverMinted.push(data[i][1]);
+// 	}
 
-	console.log(years);
-	console.log(silverMinted);
+// 	console.log(years);
+// 	console.log(silverMinted);
+
+// 	var chart = c3.generate({
+// 		bindto: '#chart',
+// 	    data: {
+// 	        columns: [
+// 	        	silverMinted
+// 	        ]
+// 	    },
+// 	    axis: {
+// 	        x: {
+// 	            type: 'category',
+// 	            categories: years,
+// 	            tick: {
+// 	            	multiline: false,
+//                 	culling: {
+//                     	max: 15
+//                 	}
+//             	}
+// 	        }
+// 	    },
+// 	    zoom: {
+//         	enabled: true
+//     	},
+// 	    legend: {
+// 	        position: 'right'
+// 	    }
+// 	});
+// }
+
+function createGraph(data){
+	var instance = ["instance"];
+	var bitcoin = ["Bitcoin"];
+	var Etherium = ["Etherium"]
+	 
+	for (var i = 1; i < data.length-1; i++) {
+		instance.push(data[i][0]);
+		bitcoin.push(data[i][1]);
+		Etherium.push(data[i][2]);
+	} 
+
+	console.log(instance);
+	console.log(bitcoin);
+	console.log(Etherium);
 
 	var chart = c3.generate({
 		bindto: '#chart',
 	    data: {
 	        columns: [
-	        	silverMinted
+				bitcoin,
+				Etherium
 	        ]
 	    },
 	    axis: {
 	        x: {
 	            type: 'category',
-	            categories: years,
+	            categories: instance,
 	            tick: {
 	            	multiline: false,
                 	culling: {
-                    	max: 15
+                    	max: 20
                 	}
             	}
 	        }
